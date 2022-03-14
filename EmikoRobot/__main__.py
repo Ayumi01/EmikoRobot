d454dc1294259c60fb6ad49ca91594ad748bd55d
Shiroyumi <101104090+Ayumi01@users.noreply.github.com>
2022-03-14 12:49:33 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -83,7 +83,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm an Ayumi Robot management bot [✨](https://telegra.ph/file/d96a232efab076d953f54.jpg)
+✪ I'm an Sajiku Robot management bot [✨](https://telegra.ph/file/b63a7ee09ce0619ec0898.jpg)
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text="About Ayumi Robot", callback_data="emiko_"),
+        InlineKeyboardButton(text="About Sajiku Robot", callback_data="emiko_"),
     ],
     [
         InlineKeyboardButton(text="Get Help", callback_data="help_back"),
@@ -103,7 +103,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="➗ Add Ayumi To Your Group ➗", url=f"t.me/{bu}?startgroup=new"),
+            text="➗ Add Sajiku To Your Group ➗", url=f"t.me/{bu}?startgroup=new"),
     ],
 ]
 
